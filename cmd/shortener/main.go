c8f48064b9f1cbe99407c892ef6f868511b843d1
Komarov Mikhail <MVKomarov@nsc.media>
2026-03-19 21:59:44 +0300

    iter1: Реализация сервиса.

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,21 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+
+	"github.com/tradekmv/shortener.git/internal/handler"
+	"github.com/tradekmv/shortener.git/internal/repository/storage"
+)
+
+func main() {
+	store := storage.New()
+	h := handler.New(store, "http://localhost:8080")
+	mux := http.NewServeMux()
+	mux.HandleFunc(`/`, h.PostHandler)
+	mux.HandleFunc(`/{id}`, h.GetHandler)
+
+	err := http.ListenAndServe(`localhost:8080`, mux)
+	if err != nil {
+		panic(err)
+	}
+}
