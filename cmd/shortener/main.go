3f058e40385912ce8d1a66e516915cc8fec5e170
Komarov Mikhail <MVKomarov@nsc.media>
2026-03-27 10:02:23 +0300

    iter3: Смена библиотеки для маршрутов.

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -3,18 +3,24 @@ package main
 import (
 	"net/http"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/tradekmv/shortener.git/internal/handler"
 	"github.com/tradekmv/shortener.git/internal/repository/storage"
 )
 
 func main() {
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+
 	store := storage.New()
 	h := handler.New(store, "http://localhost:8080")
-	mux := http.NewServeMux()
-	mux.HandleFunc(`/`, h.PostHandler)
-	mux.HandleFunc(`/{id}`, h.GetHandler)
 
-	err := http.ListenAndServe(`localhost:8080`, mux)
+	r.Post("/", h.PostHandler)
+	r.Get("/{id}", h.GetHandler)
+
+	err := http.ListenAndServe("localhost:8080", r)
 	if err != nil {
 		panic(err)
 	}
